6054ec4d6079caef8eeacb23f27a6f9130837973
kamelskipatryk <39456685+kamelskipatryk@users.noreply.github.com>
2018-07-24 19:04:23 +0200

    hiding the API key from the front-end

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'd5e7639f30ad41218958dd75fe45a25e'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with api'))
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users')
@@ -11,5 +25,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 };
